e58f6ebf159d11653b0ae43a8f2a36f2fed58002
pedroenrre <pedro_enrre@hotmail.com>
2021-04-15 11:38:37 -0300

    adds splash screen for app

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,4 +1,11 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:news_app/pages/Login.dart';
+
+import 'home.dart';
 
 class Splash extends StatefulWidget {
   @override
@@ -6,8 +13,49 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    displaySplash();
+  }
+
+  displaySplash() async {
+    Timer(Duration(seconds: 2), () async {
+      if (_auth.currentUser != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Home()),
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Login()),
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.blue,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'MobNews',
+            style: TextStyle(
+                color: Colors.grey[50],
+                fontSize: 30,
+                decoration: TextDecoration.none,
+                fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
   }
 }
